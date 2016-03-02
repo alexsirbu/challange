86c94dfa1d0b3726199420e51043de742cbe7567
Alex Sirbu <alex.sirbu.best@gmail.com>
2016-03-02 11:52:29 +0000

    work done on pulling info from reddit

--- script/script.py
+++ script/script.py
@@ -3,6 +3,7 @@ import pymongo
 from pymongo import MongoClient
 import datetime
 import time
+import praw
 
 def loop(client):
      while True:
@@ -16,4 +17,38 @@ def run():
      client = MongoClient(os.environ["MONGO_PORT_27017_TCP_ADDR"], 27017)
      loop(client)
 
-run()
+def treat_subreddit_submissions(subreddit_name, subreddit, time_limit):
+     submissions = subreddit.get_new(limit=None)
+     while True:
+         submission = submissions.next()
+         if submission.created_utc < time_limit:
+             break
+         
+         print(subreddit_name)
+         print(submission.title)
+         print(submission.created_utc)
+         
+def treat_subreddit_comments(subreddit_name, subreddit, time_limit):
+     comments = subreddit.get_comments(limit=None)
+     while True:
+         comment = comments.next()
+         if comment.created_utc < time_limit:
+             break
+
+         print(subreddit_name)
+         print(comment.body)
+         print(comment.created_utc)
+
+def treat_subreddit(subreddit_name, reddit, time_limit):
+     subreddit = reddit.get_subreddit(subreddit_name)
+     treat_subreddit_submissions(subreddit_name, subreddit, time_limit)
+     treat_subreddit_comments(subreddit_name, subreddit, time_limit)
+
+reddit = praw.Reddit(user_agent="Test Script")
+
+current_date = datetime.datetime.utcnow()
+current_timestamp = (current_date - datetime.datetime(1970,1,1)).total_seconds()
+
+treat_subreddit("python".lower(), reddit, current_timestamp - 3600*24)
+
+#run()
